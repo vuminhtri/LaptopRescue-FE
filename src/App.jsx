74328c0f9d4a2c2d346df09c21a2e87254addc0a
vuminhtri <vuminhtri290501@gmail.com>
2024-05-03 17:00:04 +0700

    new page NotFound and route to this page

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,17 @@ import { Toaster } from "react-hot-toast";
 import LoginAdmin from "./pages/LoginAdmin";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
         <Router>
             <Toaster toastOptions={{ duration: 3000 }} />
             <Routes>
-                <Route
-                    path="/"
-                    element={<Home />}
-                />
-                <Route
-                    path="/login"
-                    element={<LoginAdmin />}
-                />
-                <Route
-                    path="/admin"
-                    element={<Admin />}
-                />
-                {/* <Route path="/admin">
-                    <Route
-                        path="login"
-                        element={<LoginAdmin />}
-                    />
-                    <Route
-                        path="manage-orderlist"
-                        element={<Admin />}
-                    />
-                </Route> */}
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LoginAdmin />} />
+                <Route path="/admin" element={<Admin />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
